ba7ef06078193af574c968e8ae8191d875b8e4d4
Sachin Kumar <sjun1992@gmail.com>
2022-02-17 13:43:09 +0530

    Added basic slider component & updated title of the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Slider from './components/slider';
 
 function App() {
   return (
     <div className="App">
-      My React Project - Let's implement Slider !!
+      <Slider />
     </div>
   );
 }
---
+++ src/components/slider/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Slider extends React.Component {
+    render() {
+        return (
+            <h1>Basic Slider !!</h1>
+        );
+    }
+}
+
+export default Slider;
