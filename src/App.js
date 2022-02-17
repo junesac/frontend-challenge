adb43ee90ff1973e84371f56d6fc957d5e723198
Sachin Kumar <sjun1992@gmail.com>
2022-02-17 13:30:36 +0530

    Added React Basic Structure

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      My React Project - Let's implement Slider !!
+    </div>
+  );
+}
+
+export default App;
