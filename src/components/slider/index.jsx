925f1e9d748ed2db368ac3ad81f3b54c1910b046
Sachin Kumar <sjun1992@gmail.com>
2022-02-17 14:06:22 +0530

    Added code for basic slider.

--- src/components/slider/index.jsx
+++ src/components/slider/index.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import { useKeenSlider } from 'keen-slider/react';
+import 'keen-slider/keen-slider.min.css';
+import './slider.css';
 
-class Slider extends React.Component {
-    render() {
-        return (
-            <h1>Basic Slider !!</h1>
-        );
-    }
-}
-
-export default Slider;
+const Slider = () => {
+    const [sliderRef] = useKeenSlider({
+      loop: {
+        min: 0,
+        max: 2,
+      },
+    })
+  
+    return (
+      <div ref={sliderRef} className="keen-slider">
+        <div className="keen-slider__slide">1</div>
+        <div className="keen-slider__slide">2</div>
+        <div className="keen-slider__slide">3</div>
+      </div>
+    )
+  };
+export default Slider; 
